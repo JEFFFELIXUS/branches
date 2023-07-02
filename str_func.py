f76472fe80f9d5a8a246912bba77302b75ffd9ef
JEFFFELIXUS <dvornikov.jeff@gmail.com>
2023-07-02 16:10:29 +0300

    fix str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,7 @@
-def str_func(string):
+def to_upper(string):
     """Принимает строку и возвращает ее в верхнем регистре"""
     return string.upper()
 
-def str_func2(string):
+def to_title(string):
     """Принимает строку и возвращает ее с первой заглавной буквой у каждого слова"""
     return string.title()
